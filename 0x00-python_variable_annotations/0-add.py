b149245c53c1153c11ebef1c1201e438bcfa5c97
Just-Akinyi <justakinyi1@gmail.com>
2023-01-05 15:53:06 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,13 @@
+''' type-annotated function add'''
+#!/usr/bin/env python3
+
+
 def add(a: float, b: float) -> float:
+    '''
+    Args:
+        a: float
+        b: float
+    Return:
+        their sum as a float.
+    '''
     return a + b
