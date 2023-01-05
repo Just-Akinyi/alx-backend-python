208cb43fb1e9e9bd1cb36d6fc8cb751c07d074df
Just-Akinyi <justakinyi1@gmail.com>
2023-01-05 16:09:37 +0300

    Basic annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' type-annotated function floor'''
+from math import floor
+
+
+def floor(n: float) -> int:
+    '''
+    Args:
+        n: float
+    Return:
+         the floor of the float
+    '''
+    return floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' type-annotated function to_str'''
+
+
+def floor(n: float) -> str:
+    '''
+    Args:
+        n: float
+    Return:
+        string representation of the float
+    '''
+    return str(n)
