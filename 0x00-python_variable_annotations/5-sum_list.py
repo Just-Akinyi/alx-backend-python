996361b6100772a54d19ff0eab3f1f255ba6fcb1
Just-Akinyi <justakinyi1@gmail.com>
2023-01-05 16:39:54 +0300

    complex types

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 ''' type-annotated function sum_list'''
-from typing import Iterable
+from typing import List
 
-
-def sum_list(input_list: Iterable[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''
     Args:
         input_list: list[float]
